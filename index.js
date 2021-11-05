56a858940b3703b348ab70f96de4422ef3579686
Hridoy <rkhridoy68@gmail.com>
2021-11-05 23:02:50 +0600

    place orders routes implemented

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ async function run(){
             const features = await cursor.toArray();
             res.send(features);
         })
+
+        //Place Order
+        app.post('/submitorder' , async (req,res) => {
+            const orders = req.body;
+            const result = await orderCollection.insertOne(orders);
+            res.json(result);
+        })
     }
     finally{
 
